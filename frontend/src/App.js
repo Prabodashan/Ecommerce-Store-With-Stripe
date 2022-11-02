fb67b322a15771dac8eb09d3f4488e86adbb435c
Prabod Ashan <prabodashan14@gmail.com>
2022-11-02 15:24:33 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import NavbarComponent from "./components/NavbarComponent";
+import Store from "./pages/Store";
+import Success from "./pages/Success";
+import Cancel from "./pages/Cancel";
+
+function App() {
+  return (
+    <Container>
+      <NavbarComponent></NavbarComponent>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Store />} />
+          <Route path="success" element={<Success />} />
+          <Route path="cancel" element={<Cancel />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
+  );
+}
+
+export default App;
